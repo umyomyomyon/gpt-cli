c568821e670f649df0070962610300e62a20a2b9
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-01 18:36:21 +0900

    interactive

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,18 @@
-use clap::Parser;
+use std::io::Write;
 
-#[derive(Parser)]
-pub struct Cli {
-    pub message: String
+pub fn prompt() -> String {
+    print_head();
+    let input = read_input();
+    return input;
+}
+
+fn print_head() {
+    print!("> ");
+    std::io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).expect("Failed to read line");
+    return line.trim().to_string();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod cli;
 mod api;
 
-use clap::Parser;
-use cli::Cli;
-
 #[tokio::main]
 async fn main() {
-    let args = Cli::parse();
-    let result = api::request_chatgpt(&args.message).await.unwrap();
-    println!("{}", result.choices[0].message.content);
+    loop {
+        let input = cli::prompt();
+        let result = api::request_chatgpt(&input).await.unwrap();
+        println!("{}", result.choices[0].message.content);
+    }
 }
